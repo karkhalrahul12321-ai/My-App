94988526ca9307f9386813e817ec8b34ba9f5ebc
karkhalrahul12321-ai <karkhalrahul12321@gmail.com>
2025-12-05 17:38:28 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("App Loaded Successfully!");
+});
+
+function goToSettings() {
+  window.location.href = "settings.html";
+}
+
+function goToHome() {
+  window.location.href = "index.html";
+}
